11874c71798fa8bb438606b97c6ac994d2d30264
You-sen <88145204+You-sen@users.noreply.github.com>
2024-11-04 23:36:29 +0600

    random g

---
+++ Week-02/Day-02/rg.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k,oddC=0;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        map<char,int>mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
+        for(auto it:mp){
+            if(it.second%2!=0) oddC++;
+        }
+        cout<<(oddC>k+1?"NO":"YES")<<endl;
+    }
+    return 0;
+}

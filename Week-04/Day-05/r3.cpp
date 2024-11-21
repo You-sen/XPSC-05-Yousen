723f87f0edf88e381f92b9651c6fa28b26dcded0
You-sen <88145204+You-sen@users.noreply.github.com>
2024-11-21 23:26:29 +0600

    random 3

---
+++ Week-04/Day-05/r3.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    long long k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int l=0,r=0;
+    long long sum=0,ans=0;
+    while(r<n){
+        sum+=a[r];
+        if(sum<=k){
+            ans+=(r-l+1);
+        }
+        else{
+            while(sum>k){
+                sum-=a[l];
+                l++;
+            }
+            if(sum<=k){
+                ans+=(r-l+1);
+            }
+        }
+        r++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}

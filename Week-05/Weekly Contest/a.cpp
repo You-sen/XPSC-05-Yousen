1b51e4e969fbaa2d9e1b959a2b8caaf2d9dadd75
You-sen <88145204+You-sen@users.noreply.github.com>
2024-11-25 22:26:22 +0600

    a

---
+++ Week-05/Weekly Contest/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n,m,x,y;
+    cin>>n>>m>>x>>y;
+    cout<<(n*x)+(m*y)<<endl;
+    return 0;
+}

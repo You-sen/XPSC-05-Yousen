282360ea870520df6428d9388f8e9d13c61880d5
You-sen <88145204+You-sen@users.noreply.github.com>
2024-11-30 23:47:31 +0600

    topic wise 5

---
+++ Week-05/Day-07/t5.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fasterIO ios::sync_with_stdio(false);cin.tie(0);
+#define testCase int t=1;cin>>t;while(t--)
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define minus cout<<-1<<endl;
+
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n-1<<" "<<n<<endl;
+}
+
+int main() {
+    fasterIO
+    testCase
+    solve();
+    return 0;
+}

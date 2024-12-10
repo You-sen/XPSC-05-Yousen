cf3f8ddabc863b02e9e76e3cee74d105d7ecdb3e
You-sen <88145204+You-sen@users.noreply.github.com>
2024-12-10 22:27:39 +0600

    topic wise 3

---
+++ Week-07/Day-03/t3.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int B=30;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        vector<int>bits(B+1);
+        for(int i=0;i<n;i++){
+            for(int j=B;j>=0;j--){
+                if((a[i]>>j)&1){
+                    bits[j]++;
+                }
+            }
+        }
+        int ans=0;
+        for(int j=B;j>=0;j--){
+            if(bits[j]==n){
+                ans+=(1LL<<j);
+            }
+            else{
+                int need=n-bits[j];
+                if(k>=need){
+                    ans+=(1LL<<j);
+                    k-=need;
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}

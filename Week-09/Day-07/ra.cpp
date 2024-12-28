132440fbfcc695747d083bd94391e493fb083e03
You-sen <88145204+You-sen@users.noreply.github.com>
2024-12-28 20:10:43 +0600

    random a

---
+++ Week-09/Day-07/ra.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }
+        sort(v.begin(),v.end());
+        if(n%2==0) cout<<v[n/2]<<endl;
+        else cout<<v[n/2]<<endl;
+    }
+    return 0;
+}

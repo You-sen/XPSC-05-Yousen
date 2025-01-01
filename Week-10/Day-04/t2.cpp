f0b87caeb93e3698611cd7e607d330fab294f6ff
You-sen <88145204+You-sen@users.noreply.github.com>
2025-01-01 22:52:57 +0600

    topic wise 2

---
+++ Week-10/Day-04/t2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)cin>>v[i];
+    while(k--){
+        int key;
+        cin>>key;
+        int l=0,r=n-1,mid;
+        int index=0;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(key>=v[mid]){
+                index=mid+1;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        cout<<index<<endl;
+    }
+    return 0;
+}

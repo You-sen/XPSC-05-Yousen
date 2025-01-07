c0a1371b50cda1401a3d6451b9e76b4d29454356
You-sen <88145204+You-sen@users.noreply.github.com>
2025-01-07 22:21:26 +0600

    a

---
+++ Week-11/Practice Contest 1/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int x;
+    cin>>x;
+    cout<<(x==121?"Likely":"Unlikely")<<endl;
+    return 0;
+}

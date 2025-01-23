d2b9b83c5fd5f9e5a39df1a96858e24b41b34048
You-sen <88145204+You-sen@users.noreply.github.com>
2025-01-24 01:26:01 +0600

    random c

---
+++ Week-13/Day-02/rc.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fasterIO ios::sync_with_stdio(false);cin.tie(0);
+#define testCase int t=1;cin>>t;while(t--)
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define minus cout<<-1<<endl;
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    string a,b;
+    cin>>a>>b;
+    int ans=0;
+    int j=0;
+    for(int i=0;i<m&&j<n;i++){
+        if(a[j]==b[i]){
+            j++;
+        }
+    }
+    cout<<j<<endl;
+}
+
+int main() {
+    fasterIO
+    testCase
+    solve();
+    return 0;
+}

b801173665310d8c00843ddd3fad11a7a002f1c8
You-sen <88145204+You-sen@users.noreply.github.com>
2025-01-27 21:16:31 +0600

    random e

---
+++ Week-13/Day-05/re.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fasterIO ios::sync_with_stdio(false);cin.tie(0);
+#define testCase int t=1;cin>>t;while(t--)
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define minus cout<<-1<<endl;
+
+void solve(){
+    int x,n,m;
+    cin>>x>>n>>m;
+    
+    for(int i=0;i<n;i++){
+        if((x/2)+10>x)break;
+        x=(x/2)+10;
+    }
+    x-=(m*10);
+    if(x<=0)yes
+    else no
+}
+
+int main() {
+    fasterIO
+    testCase
+    solve();
+    return 0;
+}

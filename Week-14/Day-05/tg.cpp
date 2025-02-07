a52bed97360558d82d21b572138759d42af025ae
You-sen <88145204+You-sen@users.noreply.github.com>
2025-02-07 23:45:10 +0600

    topicwise g

---
+++ Week-14/Day-05/tg.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;
+template<typename T>using pbds=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        long long ans=0;
+        pbds<int>p;
+        for(int i=n-1;i>=0;i--){
+            ans+=p.order_of_key(a[i]);
+            p.insert(a[i]);
+        }
+        cout<<ans<<endl;
+
+    }
+    return 0;
+}
+/*
+we will traverse in reverse way and each time we will insert it in pbds set and check order of key
+*/
